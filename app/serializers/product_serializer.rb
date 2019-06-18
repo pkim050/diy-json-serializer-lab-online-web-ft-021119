62b6d2d65869e552ed82871773cf407e5b2731a2
Patrick Kim <pkim050@gmail.com>
2019-06-18 14:01:22 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,7 @@
-class PostSerializer
-  def self.serialize(post)
-    
+class ProductSerializer
+  def self.serialize(product)
+    serialized_product = '{'
+
+    serialized_product += '"id": ' + product.id.to_s + ', '
+    serialized_product += '"name": "' + product.name + '", '
+    serialized_product += '"price": "' + product.price + '", '
