5c731721e7f2e023f2870b779bb7ca44ca38522b
Patrick Kim <pkim050@gmail.com>
2019-06-18 14:23:45 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products, only: [:index, :new, :create]
+  resources :products, only: [:index, :new, :create, :show]
   get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
   get '/prodcuts/:id/product_data', to: 'products#product_data'
